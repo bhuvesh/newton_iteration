53cae36ef4292f11a14f38e6db7c6e53fb465486
Bhuvesh Kumar <bhuvesh@cse.iitk.ac.in>
2017-10-19 10:45:39 -0400

    some lines

--- drawline.m
+++ drawline.m
@@ -1,12 +1,12 @@
 function drawline(points,withp)
 
 for iter=1:size(points,1)-1
-    line([points(iter,1),points(iter+1,1)],[points(iter,2),points(iter+1,2)],'Color','white','LineWidth',2);
+    line([points(iter,1),points(iter+1,1)],[points(iter,2),points(iter+1,2)],'Color','k','LineWidth',2);
 end
 
 if (withp)
     marksize=3;
     for iter=1:size(points,1)
-    rectangle('Position',[points(iter,1) - marksize,points(iter,2) - marksize,2*marksize,2*marksize],'Curvature',[1,1], 'LineWidth',1,'Facecolor','k','EdgeColor',[0 0 0]);
+    rectangle('Position',[points(iter,1) - marksize,points(iter,2) - marksize,2*marksize,2*marksize],'Curvature',[1,1], 'LineWidth',1,'Facecolor',[1 1 1]*(iter/size(points,1)),'EdgeColor',[1 1 1]*(iter/size(points,1)));
     end
 end
